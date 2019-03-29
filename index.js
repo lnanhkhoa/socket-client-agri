b8ee5334ccd0674c6761c4278e9b96de0d0e68be
lnanhkhoa <lnanhkhoa303@gmail.com>
2019-03-29 14:17:44 +0700

    dqediji

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const socket = require('socket.io-client')('http://localhost:1234');
 const fetch = require('node-fetch');
-
+const _ = require('lodash')
 
 
 socket.on('connect', function () {
@@ -16,3 +16,21 @@ socket.on('disconnect', function () {
     console.log('disconnect')
 
 });
+
+
+
+fetch('http://172.16.0.249:8080/api/clients/Contiki-NG-1.04B0005AF4C18/3301/0?format=TLV')
+    .then(res => res.json())
+    .then(body => {
+        const list_value = body.content.resources;
+        // console.log(list_value)
+        const value5700 = _.find(list_value, 
+            function(i) {
+            return (i.id === 5700)
+        })
+        console.log(value5700)
+
+    });
+
+
+    
