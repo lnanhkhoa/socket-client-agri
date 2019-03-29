6d237f7067724e9d435343df2ac43822b59bd713
An <hoanan37@gmail.com>
2019-03-29 15:21:00 +0700

    Read sensor value 4c18 and 4BFB

--- index.js
+++ index.js
@@ -18,19 +18,57 @@ socket.on('disconnect', function () {
 });
 
 
-
-fetch('http://172.16.0.249:8080/api/clients/Contiki-NG-1.04B0005AF4C18/3301/0?format=TLV')
+//Get Lux value of 4c18
+fetch('http://localhost:8080/api/clients/Contiki-NG-1.04B0005AF4C18/3301/0/5700?format=TLV')
     .then(res => res.json())
     .then(body => {
-        const list_value = body.content.resources;
+        const Lux_4C18 = body.content.value;
         // console.log(list_value)
-        const value5700 = _.find(list_value, 
-            function(i) {
-            return (i.id === 5700)
-        })
-        console.log(value5700)
+        // const value5700 = _.find(list_value, 
+        //     function(i) {
+        //     return (i.id === 5700)
+        // })
+        console.log(Lux_4C18)
 
     });
+//Get Air temp of 4c18
+fetch('http://localhost:8080/api/clients/Contiki-NG-1.04B0005AF4C18/3303/0/5700?format=TLV')
+    .then(res => res.json())
+    .then(body => {
+        const Air_temp_4C18 = body.content.value;
+        // console.log(list_value)
+        // const value5700 = _.find(list_value, 
+        //     function(i) {
+        //     return (i.id === 5700)
+        // })
+        console.log(Air_temp_4C18)
+        //console.log("Air temp")
 
+    });
+//Get Air humidity of 4c18
+fetch('http://localhost:8080/api/clients/Contiki-NG-1.04B0005AF4C18/3304/0/5700?format=TLV')
+    .then(res => res.json())
+    .then(body => {
+        const Air_humidity_4C18 = body.content.value;
+        // console.log(list_value)
+        // const value5700 = _.find(list_value, 
+        //     function(i) {
+        //     return (i.id === 5700)
+        // })
+        console.log(Air_humidity_4C18)
+
+    });
+fetch('http://localhost:8080/api/clients/Contiki-NG-1.04B0005AF4BFB/3301/0/5700?format=TLV')
+    .then(res => res.json())
+    .then(body => {
+        const Lux_4BFB = body.content.value;
+        // console.log(list_value)
+        // const value5700 = _.find(list_value, 
+        //     function(i) {
+        //     return (i.id === 5700)
+        // })
+        console.log(Lux_4BFB)
+
+    });
 
     
